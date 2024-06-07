862680ca9b45551ab33e807f35319845d6b9cbed
Ben Anderson <andersjbe@gmail.com>
2024-06-07 11:34:58 -0400

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -16,9 +16,23 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	buf := make([]byte, 1024)
+	_, err = conn.Read(buf)
+	if err != nil {
+		fmt.Printf("Error while reading request: %s\n", err.Error())
+	}
+
+	resp := "HTTP/1.1 200 OK\r\n\r\n"
+	_, err = conn.Write([]byte(resp))
+	if err != nil {
+		fmt.Printf("Error while writing response: %s\n", err.Error())
+	}
+
+	conn.Close()
 }
